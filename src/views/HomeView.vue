e88b8b4ea3ee629943ff9311201e9ee5c0b1a6e0
Gloria <figmabebe@gmail.com>
2023-11-25 13:21:33 +0700

    filter project feature

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,24 +1,28 @@
 <template>
   <div class="home">
     <h1>Home</h1>
-    <div v-for="project in projects" :key="project.id">
+    <FilterNav @filterValue="current=$event" :current="current"></FilterNav>
+    <div v-for="project in filteredProjects" :key="project.id">
        <SingleProject :project="project"  @delete="deleteProject" @complete="completeProject"></SingleProject>
     </div>
   </div>
 </template>
 
 <script>
+import FilterNav from '../components/FilterNav'
 import SingleProject from '@/components/SingleProject.vue'
 // @ is an alias to /src
 
 export default {
   name: 'HomeView',
   components:{
+    FilterNav,
     SingleProject,
   },
   data(){
     return{
-      projects:[] //{}
+      projects:[],
+      current:"all" //{}
     }
   },
   methods:{
@@ -32,6 +36,21 @@ export default {
         return project.id===id;
       });
       findProject.complete=!findProject.complete;
+    }
+  },
+  computed:{
+    filteredProjects(){
+      if(this.current==="complete"){
+        return this.projects.filter((p)=>{
+          return p.complete
+        })
+      }
+      if(this.current==="ongoing"){
+        return this.projects.filter((p)=>{
+          return !p.complete
+        })
+      }
+    return this.projects;
     }
   },
   mounted(){
